59077e07c2ef570c3c4ed2edce8ff5e38f93f8f7
Agnes Bolton <Agnes-B@users.noreply.github.com>
2025-06-07 23:35:14 +0100

    Create div array and log mouseenter

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,1 +1,19 @@
-console.log("Hello world!");
+const container = document.querySelector('#container');
+
+for (let i = 0; i < 16; i++) {
+    const row = document.createElement('div');
+    row.setAttribute("class", "row");
+    for (let j = 0; j < 16; j++) {
+        const div = document.createElement('div');
+        div.setAttribute("class", "square");
+        row.appendChild(div);
+    }
+    container.appendChild(row);
+}
+
+function mouseenter(e) {
+    console.log(e);
+}
+
+const squares = document.querySelectorAll(".square");
+squares.forEach(square => square.addEventListener('mouseover', mouseenter));
